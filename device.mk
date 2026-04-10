e0af13e4a51ddc03484bd701825f71d56b9187cd
zenin1504 <kayozzprojects@yandex.com>
2026-04-11 04:13:51 +0800

    peridot: Import device features on tree
    Change-Id: I0c932ab075454d878a90f80b20eb41c2
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Device-Features
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/device_features/peridot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/peridot.xml
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
